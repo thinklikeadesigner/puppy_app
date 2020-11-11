8ab4cbea75667c32de603a4ca96dc506e8fb34d8
rebecca burch <rebecca.burch.stu@gmail.com>
2020-11-11 17:45:37 -0600

    added lib file

---
+++ lib/screens/locations/locations.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../locations/tile_overlay.dart';
+import '../../widgets/image_banner.dart';
+import '../../app.dart';
+import '../../models/location.dart';
+
+class Locations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Locations'),
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) =>
+            _itemBuilder(context, locations[index]),
+      ),
+    );
+  }
+
+  _onLocationTap(BuildContext context, int locationID) {
+    Navigator.pushNamed(context, LocationDetailRoute,
+        arguments: {"id": locationID});
+  }
+
+  Widget _itemBuilder(BuildContext context, Location location) {
+    return GestureDetector(
+      onTap: () => _onLocationTap(context, location.id),
+      child: Container(
+        height: 245,
+        child: Stack(children: [
+          ImageBanner(assetPath: location.imagePath, height: 245.0),
+          TileOverlay(location),
+        ]),
+      ),
+    );
+  }
+}
